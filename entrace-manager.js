1958cc8f5baf02c5c3057294c58f7304d765f58b
Elton Henrique Faust <elton.h.faust@gmail.com>
2017-10-29 22:05:04 -0200

    Alterações até 29/10

--- entrace-manager.js
+++ entrace-manager.js
@@ -12,6 +12,18 @@ app.use((req, res, next) => {
     next();
 });
 
+app.get('/img/:user.jpg', (req, res, next) => {
+    res.sendFile(
+        'data/project_dataset/raw/' + req.params.user.replace(/[^\d]/g, '') + '/identifier.jpg',
+        {root: __dirname + '/', dotfiles: 'deny',},
+        (err) => {
+            if (err) {
+                  res.sendStatus(404);
+            }
+        }
+    );
+});
+
 const server = require('http').Server(app);
 
 const WebSocket = require('ws');
@@ -22,93 +34,97 @@ const wsIdentifier = new WebSocket(process.env.PERSON_IDENTIFIER_MANAGER_WS);
 
 let users = {};
 let entraces = {};
+let occourences = [];
 
 console.log('Pre setting available entraces');
 
 let entraceNames = process.env.ENTRACE_MANAGER_ENTRACES_NAMES.split(',');
 process.env.ENTRACE_MANAGER_ENTRACES_IDS.split(',').forEach((entrace_id, i) => {
-	console.log('Set entrace %s with identifier %s', entraceNames[i], entrace_id);
+    console.log('Set entrace %s with identifier %s', entraceNames[i], entrace_id);
 
-	entraces[entrace_id] = {
-		name: entraceNames[i],
-		isBlocked: true,
-		users: [],
-	};
+    entraces[entrace_id] = {
+        name: entraceNames[i],
+        isBlocked: true,
+        users: [],
+    };
 });
 
 const sendToWS = (ws, type, data) => {
-	data = data || {};
-	data.type = type;
+    data = data || {};
+    data.type = type;
 
-	ws.send(JSON.stringify(data));
+    ws.send(JSON.stringify(data));
 }
 
 wssUsers.on('connection', (ws) => {
-	console.log('User connected');
+    console.log('User connected');
 
-	ws.on('message', (data) => {
-		data = JSON.parse(data);
+    ws.on('message', (data) => {
+        data = JSON.parse(data);
 
-		if (typeof users[data.user_id] == 'undefined') {
-			console.log('User set for id %s', data.user_id);
+        if (typeof users[data.user_id] == 'undefined') {
+            console.log('User set for id %s', data.user_id);
 
-			users[data.user_id] = {
-				client: ws,
-				name: ''
-			};
-		}
+            users[data.user_id] = {
+                client: ws,
+                name: ''
+            };
+        }
 
-		switch(data.type) {
-			case 'identify_as':
-				users[data.user_id].name = data.name.
-				break;
-			case 'list_entraces':
-				let availEntraces = [];
+        switch(data.type) {
+            case 'identify_as':
+                users[data.user_id].name = data.name.
+                break;
+            case 'list_entraces':
+                let availEntraces = [];
 
-				for (let i in entraces) {
-					availEntraces.push({id: i, name: emtraces[i].name});
-				}
+                for (let i in entraces) {
+                    availEntraces.push({id: i, name: entraces[i].name});
+                }
 
-				sendToWS(ws, 'list_of_entraces', {list: availEntraces});
-				break;
-			case 'join_entrace':
-				if (entraces[data.entrace_id].users.indexOf(data.user_id) == -1) {
-					entraces[data.entrace_id].users.push(data.user_id);
-				}
+                sendToWS(ws, 'list_of_entraces', {list: availEntraces});
+                break;
+            case 'list_occourences':
+                sendToWS(ws, 'list_of_occourences', {list: occourences});
+                break;
+            case 'join_entrace':
+                if (entraces[data.entrace_id].users.indexOf(data.user_id) == -1) {
+                    entraces[data.entrace_id].users.push(data.user_id);
+                }
 
-				sendToWS(ws, 'joined_entrace', {id: data.entrace_id});
-				break;
-			case 'leave_entrace':
-				let idx = entraces[data.entrace_id].users.indexOf(data.user_id);
+                sendToWS(ws, 'joined_entrace', {id: data.entrace_id});
+                break;
+            case 'leave_entrace':
+                let idx = entraces[data.entrace_id].users.indexOf(data.user_id);
 
-				if (idx != -1) {
-					entraces[data.entrace_id].users.splice(idx);
-				}
+                if (idx != -1) {
+                    entraces[data.entrace_id].users.splice(idx);
+                }
 
-				sendToWS(ws, 'leaved_entrace', {id: data.entrace_id});
-				break;
-		}
-	});
+                sendToWS(ws, 'leaved_entrace', {id: data.entrace_id});
+                break;
+        }
+    });
 
-	ws.on('error', (e) => {
-		console.log('%s on wssUsers', e.message)
-	})
+    ws.on('error', (e) => {
+        console.log('%s on wssUsers', e.message)
+    })
 });
 
 wssUsers.on('error', (e) => {
-	console.log('%s on wsIdentifier', e.message)
+    console.log('%s on wsIdentifier', e.message)
 })
 
 wsIdentifier.on('open', () => {
-	console.log('Manager connected to identifier');
+    console.log('Manager connected to identifier');
 });
 
 wsIdentifier.on('message', (data) => {
-	data = JSON.parse(data);
+    data = JSON.parse(data);
 });
 
 wsIdentifier.on('error', (e) => {
-	console.log('%s on wsIdentifier', e.message)
+    console.log('%s on wsIdentifier', e.message)
 })
 
 server.listen(3000, () => {
